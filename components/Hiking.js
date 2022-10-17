08d5deed44f52cd04121fdb5819b2a36e53ce59e
yafet-123 <yafetaddisu123@gmail.com>
2022-10-17 14:15:50 +0300

    adding hiking

--- components/Hiking.js
+++ components/Hiking.js
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export function Hiking() {
   return (
-    <div className="bg-black w-full h-full px-20 pb-24">
-      <div className="flex flex-col">
+    <div className="bg-black w-full h-full px-0 md:px-20 pb-24">
+      <div className="flex flex-col px-20 md:px-0">
         <h1 className="text-white text-5xl text-left font-bold uppercase mb-5 tracking-wide">
           Hiking in Ethopia
         </h1>
         -
-        <p className="text-white font-normal w-1/2 tracking-wide text-base mb-5">
+        <p className="text-white font-normal w-full lg:w-1/2 tracking-wide text-base mb-5">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet risus
           nullam eget felis eget nunc lobortis Ut enim ad minim veniam, quis
@@ -21,28 +21,28 @@ export function Hiking() {
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        <div className="flex flex-col justify-between">
+        <div className="flex flex-col justify-between ">
           <Image src="/images/bgImage12.avif" width={250} height={250} />
-          <p className="text-white font-normal w-3/4 tracking-wide text-base my-5">
+          <p className="text-white font-normal w-full lg:w-3/4 tracking-wide text-base my-5 px-20 md:px-0">
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur.
           </p>
-          <button className="p-3 bg-yellow-500 capitalize text-lg w-1/4 text-white font-bold">
+          <button className="p-3 bg-yellow-500 capitalize text-lg w-1/4 text-white font-bold mx-20 md:mx-0">
             Read more
           </button>
         </div>
 
         <div className="flex flex-col justify-between">
           <Image src="/images/bgImage13.avif" width={250} height={250} />
-          <p className="text-white font-normal w-3/4 tracking-wide text-base my-5">
+          <p className="text-white font-normal w-full lg:w-3/4 tracking-wide text-base my-5 px-20 md:px-0">
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur.
           </p>
-          <button className="p-3 bg-yellow-500 capitalize text-lg w-1/4 text-white font-bold">
+          <button className="p-3 bg-yellow-500 capitalize text-lg w-1/4 text-white font-bold mx-20 md:mx-0">
             Read more
           </button>
         </div>
--- components/RighThings.js
+++ components/RighThings.js
@@ -2,15 +2,54 @@ import React from "react";
 
 export function RighThings() {
   return (
-    <div className="RighThings-background-one w-full h-[30rem] pt-10 bg-black">
-      <h1 className="text-white text-5xl text-left font-bold uppercase mb-5 tracking-wide">
-        Hiking in Ethopia
-      </h1>
-      -
-      <p className="text-white font-normal w-1/2 tracking-wide text-base mb-5">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
+    <div className="flex flex-col h-full w-full">
+      <div className="RighThings-background-one w-full h-full md:h-[30rem] pt-10 bg-black px-20">
+        <h1 className="text-white text-5xl text-left font-bold uppercase mb-5 tracking-wide">
+          Hiking in Ethopia
+        </h1>
+        -
+        <p className="text-white font-normal w-2/4 tracking-wide text-xl mb-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+          <div>
+            <h1 className="text-white text-9xl font-bold uppercase text-center">
+              37
+            </h1>
+            <p className="text-white font-normal tracking-wide text-xl mb-5 text-center">
+              Lorem
+            </p>
+          </div>
+
+          <div>
+            <h1 className="text-white text-9xl font-bold uppercase text-center">
+              65
+            </h1>
+            <p className="text-white font-normal tracking-wide text-xl mb-5 text-center">
+              Lorem
+            </p>
+          </div>
+
+          <div>
+            <h1 className="text-white text-9xl font-bold uppercase text-center">
+              24
+            </h1>
+            <p className="text-white font-normal tracking-wide text-xl mb-5 text-center">
+              Lorem
+            </p>
+          </div>
+
+          <div>
+            <h1 className="text-white text-9xl font-bold uppercase text-center">
+              85
+            </h1>
+            <p className="text-white font-normal tracking-wide text-xl mb-5 text-center">
+              Lorem
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
