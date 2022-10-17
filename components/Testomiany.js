cba9b9a1b9684a6bc1ef6db8c32454385b05149f
yafet-123 <yafetaddisu123@gmail.com>
2022-10-17 14:51:55 +0300

    adding hiking

--- components/Testomiany.js
+++ components/Testomiany.js
@@ -38,7 +38,21 @@ export function Testomiany() {
       <div className="testomiany-background-three w-full h-[30rem] mb-10"></div>
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mb-10 px-32">
-        <div className=""></div>
+        <div className="flex flex-col justify-between px-20 md:px-0">
+          <p className="text-white font-normal w-3/4 tracking-wide text-base mb-5">
+            Denbel city mall
+          </p>
+          <p className="text-white font-normal w-3/4 tracking-wide text-base mb-5">
+            Bole subciy Around Denbel
+          </p>
+          <p className="text-white font-normal w-3/4 tracking-wide text-base mb-5">
+            +251934781038
+          </p>
+
+          <p className="text-white font-normal w-3/4 tracking-wide text-base mb-5">
+            yafetaddisu123@gmail.com
+          </p>
+        </div>
         <div className="flex flex-col justify-between px-20 md:px-0">
           <h1 className="text-white text-4xl text-left font-bold uppercase mb-5 tracking-wide">
             take a hike
